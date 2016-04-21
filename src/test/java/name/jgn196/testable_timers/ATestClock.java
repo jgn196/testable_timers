c161f642d375dfa73956b265538f6909a3f17a25
John Nash <john.nash@capgemini.com>
2016-04-21 20:13:16 +0100

    Added a test for the TestClock.

--- src/test/java/name/jgn196/testable_timers/ATestClock.java
+++ src/test/java/name/jgn196/testable_timers/ATestClock.java
@@ -1,7 +1,21 @@
 package name.jgn196.testable_timers;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-public class ATestClock {
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class ATestClock {
+    @Test
+    public void signalsRegisteredListenersWhenTriggered() {
+        final TestClock clock = new TestClock();
+
+        final AtomicInteger ticksHeard = new AtomicInteger(0);
+        final Clock.Listener listener = () -> ticksHeard.getAndAdd(1);
+
+        clock.register(listener);
+        clock.triggerTimer();
+
+        Assert.assertEquals(1, ticksHeard.get());
+    }
 }
